136dd8b24289fb5cd6ff540fee4d0601eda93999
Srinibas Misra <srinibas.misra97@gmail.com>
2025-09-20 18:32:28 +0300

    added a lot

--- src/message-processor/app.py
+++ src/message-processor/app.py
@@ -1,9 +1,13 @@
 from flask import Flask, request, jsonify
 from cloudevents.http import from_http
 import json
-import os
+import requests
 
 app = Flask(__name__)
+
+STATESTORE="datastore"
+
+URL = "http://localhost:3500/v1.0/state/%s" % (STATESTORE)
 
 # Register Dapr pub/sub subscriptions
 @app.route('/dapr/subscribe', methods=['GET'])
@@ -22,8 +26,14 @@ def subscribe():
 def orders_subscriber():
     event = from_http(request.headers, request.get_data())
     print('Subscriber received : %s' % event.data['orderId'], flush=True)
-    return json.dumps({'success': True}), 200, {
-        'ContentType': 'application/json'}
+    response = requests.post(
+        URL,
+        params={"metadata.contentType": "application/json"},
+        json=[{"key": "orderId", "value": event.data['orderId']}],
+        headers={"Content-Type": "application/json"}
+    )
+
+    return "success",response.status_code
 
 
 if __name__=="__main__":
